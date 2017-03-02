cdf392d1ceb6c8541770ac59421b2ecdbc116501
Maximilian Münchow <maximilian.muenchow@teamspeak.com>
2017-03-02 02:49:14 +0100

    disabled warning 1591 for LibraryVersion

--- source/Enums/LibraryVersion.cs
+++ source/Enums/LibraryVersion.cs
@@ -5,7 +5,9 @@
     /// </summary>
     public enum LibraryVersion: ulong
     {
+#pragma warning disable 1591
         V_3_0_3_2 = 1433933257,
         V_3_0_4 = 1488208851,
+#pragma warning restore 1591
     }
 }
